e46e9a9668b772b20e4b8a8fd40aac5b7893f2d6
SANGEETA <sangeeta.69b1@gmail.com>
2015-06-15 13:34:27 +0530

    adding lower_limit and upper_limit

--- regression1/src/regression1/combine_all_features_update_rank.py
+++ regression1/src/regression1/combine_all_features_update_rank.py
@@ -8,6 +8,8 @@ import MySQLdb
 
 project   = "chromium"
 
+lower_limit = 0
+upper_limit = 10
 #"""
 port=3307
 user="sangeetal"
@@ -31,14 +33,14 @@ db1= MySQLdb.connect(host="localhost",user=user, passwd=password, db=database, p
 select_cursor = db1.cursor()
 update_cursor  = db1.cursor()
 
-revid_str = "select distinct revid from " + revid_feature_table
+revid_str = "select distinct revid from " + revid_feature_table  +"  limit "+ lower_limit+","+ upper_limit
 select_cursor.execute(revid_str)
 revid_data = select_cursor.fetchall()
 
 
 for rev in revid_data:
     feature_str = "select rev_log_message, is_bug_fix, changed_path_count, lines_added, lines_deleted, lines_changed, chunks_added, chunks_deleted, chunks_modified, churn, changed_files, test_file_count, \
-                   day, month, weakday, hour, max_dev_in_file, max_change_count, avg_rev_comitter_expr  from " +  revid_feature_table+ " where revid = "+ (str)(rev[0])
+                   day, month, weakday, hour, max_dev_in_file, max_change_count, avg_rev_comitter_expr  from " +  revid_feature_table+ " where revid = "+ (str)(rev[0]) 
     
     print "revid=",rev[0] 
     select_cursor.execute(feature_str)
