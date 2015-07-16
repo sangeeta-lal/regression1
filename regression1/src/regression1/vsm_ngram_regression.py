e8a0d08a5063ed86492cac745e141d9871a40eae
SANGEETA <sangeeta.69b1@gmail.com>
2015-07-16 12:23:29 +0530

    debug

--- regression1/src/regression1/vsm_ngram_regression.py
+++ regression1/src/regression1/vsm_ngram_regression.py
@@ -20,8 +20,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 project   = "chromium"
+model =  "VEC"
 
-#"""
+"""
 port=3307
 user="sangeetal"
 password="sangeetal"
@@ -29,7 +30,8 @@ database="regression1"
 bug_report_feature_table = project + "_bug_report_features"
 bugid_previous_30_days_revids_table = project +"_bugid_previous_30day_revids"
 revid_feature_table = project+"_revids_feature"
-bugid_revid_table = project+"_bugid_reg_revids"
+train_bugid_revid_table = project+"_train_bugid_reg_revids"
+test_bugid_revid_table = project+"_test_bugid_reg_revids"
 #score_table= project+"_score_table"
 combined_score_table = project+"_combined_score_table"
 """
@@ -40,7 +42,8 @@ database="regression1"
 bug_report_feature_table = project + "_bug_report_features"
 bugid_previous_30_days_revids_table = project +"_bugid_previous_30day_revids"
 revid_feature_table = project+"_revids_feature"
-bugid_revid_table = project+"_bugid_reg_revids"
+train_bugid_revid_table = project+"_train_bugid_reg_revids"
+test_bugid_revid_table = project+"_test_bugid_reg_revids"
 #score_table= project+"_score_table"
 combined_score_table = project+"_combined_score_table"
 #"""
@@ -88,7 +91,10 @@ def get_rank(title_rev_log_sim_matrix, desc_rev_log_sim_matrix, cr_area_top_leve
             rank =  rank+1
         
     return rank
-        
+  
+  
+#========================TRAINING PART==============================#
+#===================================================================#        
 w1=-0.1
 w2 =- 0.1
 w3 =-0.1
@@ -109,134 +115,132 @@ while w1 <=0.9:
                 if(w1+w2+w3+w4)==1.0:
                     print "yes"
                  
+
+                    total_sim_reg_causing=0.0
                 
+                    str_bug = "select distinct bugid   from " + train_bugid_revid_table
+                    select_cursor.execute(str_bug)
+                    bug_data =  select_cursor.fetchall()
+                    
+                    print "@debug: Total bugs=", len(bug_data)
 
-w1 = 0.2
-w2 = 0.3
-w3 = 0.4
-w4=  0.1
-
-total_sim_reg_causing=0.0
-                
-str_bug = "select distinct bugid   from chromium_bugid_reg_revids"
-select_cursor.execute(str_bug)
-bug_data =  select_cursor.fetchall()
-
-for id in bug_data:
-    total_bugs =  total_bugs+1
-    title_rev_log =  list()
-    desc_rev_log = list()
-    cr_area_top_level=list()
-    title_file_name = list()
+                    for id in bug_data:
+                        total_bugs =  total_bugs+1
+                        title_rev_log =  list()
+                        desc_rev_log = list()
+                        cr_area_top_level=list()
+                        title_file_name = list()
     
-    bugid = id[0]
+                        bugid = id[0]
     
-    str_bug_info = "select title, description, cr, area from  "+bug_report_feature_table+ " where bugid="+(str)(bugid)
-    print "bugid=", bugid, "  total bugs=", total_bugs,  "  found=", total_revids_found
-    select_cursor.execute(str_bug_info)
-    bug_feature_info  =  select_cursor.fetchall()
+                        str_bug_info = "select title, description, cr, area from  "+bug_report_feature_table+ " where bugid="+(str)(bugid)
+                        print "bugid=", bugid, "  total bugs=", total_bugs,  "  found=", total_revids_found
+                        break
+                        select_cursor.execute(str_bug_info)
+                        bug_feature_info  =  select_cursor.fetchall()
     
-    bug_title = bug_feature_info[0][0]
-    bug_desc = bug_feature_info[0][1]
-    bug_cr =  bug_feature_info[0][2]
-    bug_area = bug_feature_info[0][3]
+                        bug_title = bug_feature_info[0][0]
+                        bug_desc = bug_feature_info[0][1]
+                        bug_cr =  bug_feature_info[0][2]
+                        bug_area = bug_feature_info[0][3]
     
-    bug_cr_and_area =  bug_cr+ " "+ bug_area
-    #bug_all_features =  bug_title+ " "+ bug_description+ " "+ bug_cr+" "+ bug_area
-    #bug_all_feature =  utill.clean(bug_all_features)
-    print " title", bug_title, "  dec=", bug_desc, "  cr=",bug_cr, " area=", bug_area
+                        bug_cr_and_area =  bug_cr+ " "+ bug_area
+                        #bug_all_features =  bug_title+ " "+ bug_description+ " "+ bug_cr+" "+ bug_area
+                        #bug_all_feature =  utill.clean(bug_all_features)
+                        print " title", bug_title, "  dec=", bug_desc, "  cr=",bug_cr, " area=", bug_area
     
-    ## utf 8 encoding
-    #bug_title = bug_title.encode('utf8')   @Not working
-    title_rev_log.append(bug_title)     
-    desc_rev_log.append(bug_desc)
-    cr_area_top_level.append(bug_cr_and_area)
-    title_file_name.append(bug_title)
+                        ## utf 8 encoding
+                        #bug_title = bug_title.encode('utf8')   @Not working
+                        title_rev_log.append(bug_title)     
+                        desc_rev_log.append(bug_desc)
+                        cr_area_top_level.append(bug_cr_and_area)
+                        title_file_name.append(bug_title)
     
-    candidate_revid_list = list()   
+                        candidate_revid_list = list()   
     
-    str_revs =   "select revid, reg_causing from "+ bugid_previous_30_days_revids_table +"  where  bugid="+(str)(bugid)
-    #print "str revs=", str_revs
-    select_cursor.execute(str_revs)
-    temp_revs = select_cursor.fetchall()
+                        str_revs =   "select revid, reg_causing from "+ bugid_previous_30_days_revids_table +"  where  bugid="+(str)(bugid)
+                        #print "str revs=", str_revs
+                        select_cursor.execute(str_revs)
+                        temp_revs = select_cursor.fetchall()
     
-    reg_causing_revid = 0
-    for r in temp_revs:
-        #print "r is =",r
-        candidate_revid_list.append(r[0])  
+                        reg_causing_revid = 0
+                        for r in temp_revs:
+                            #print "r is =",r
+                            candidate_revid_list.append(r[0])  
                
-        if r[1]==1:
-            reg_causing_revid = r[0]  
+                            if r[1]==1:
+                                reg_causing_revid = r[0]  
     
-    #Identify reg-causing revids and their log message
-    reg_causing_revid_pos = 0
-    count = 0
-    for temp_rev in candidate_revid_list:
-       # temp_rev=3017
-        str_rev_info = "select  rev_log_message, changed_files from "+revid_feature_table+ " where revid="+(str)(temp_rev)
-        #print "str=", str_rev_info
-        select_cursor.execute(str_rev_info)
-        rev_info  =  select_cursor.fetchall()
+                        #Identify reg-causing revids and their log message
+                        reg_causing_revid_pos = 0
+                        count = 0
+                        for temp_rev in candidate_revid_list:
+                            #temp_rev=3017
+                            str_rev_info = "select  rev_log_message, changed_files from "+revid_feature_table+ " where revid="+(str)(temp_rev)
+                            #print "str=", str_rev_info
+                            select_cursor.execute(str_rev_info)
+                            rev_info  =  select_cursor.fetchall()
+                            
+                            print " rev info = ", rev_info
+                            rev_log_mess =  rev_info[0][0]
+                            changed_files_with_path=  rev_info[0][1]
         
-        rev_log_mess =  rev_info[0][0]
-        changed_files_with_path=  rev_info[0][1]
-        
-        changed_files_arr=[]
-        changed_files_arr = changed_files_with_path.split("\n")
-        changed_file_names = ""
-        top_level_names = ""
-        i=0
-        while i< len(changed_files_arr):
-            dir_depth= len(changed_files_arr[i].split("/"))
-            changed_file_names =changed_files_arr[i].split("/")[dir_depth-1].split(".")[0] +" "+changed_file_names
-            top_level_names = changed_files_arr[i].split("/")[dir_depth-2] +" "+top_level_names
-            i=i+1
+                            changed_files_arr=[]
+                            changed_files_arr = changed_files_with_path.split("\n")
+                            changed_file_names = ""
+                            top_level_names = ""
+                            i=0
+                            while i< len(changed_files_arr):
+                                dir_depth= len(changed_files_arr[i].split("/"))
+                                changed_file_names =changed_files_arr[i].split("/")[dir_depth-1].split(".")[0] +" "+changed_file_names
+                                top_level_names = changed_files_arr[i].split("/")[dir_depth-2] +" "+top_level_names
+                                i=i+1
             
-       # print " log mess", rev_log_mess, "  files=", changed_files_with_path,  " fie name=", changed_file_names, " \n top level", top_level_names
-        ## utf 8 encoding
-        #rev_log_mess = rev_log_mess.encode('utf8') 
-        title_rev_log.append(rev_log_mess)
-        desc_rev_log.append(rev_log_mess)
-        cr_area_top_level.append(top_level_names)
-        title_file_name.append(changed_file_names)
+                            #print " log mess", rev_log_mess, "  files=", changed_files_with_path,  " fie name=", changed_file_names, " \n top level", top_level_names
+                            ## utf 8 encoding
+                            #rev_log_mess = rev_log_mess.encode('utf8') 
+                            title_rev_log.append(rev_log_mess)
+                            desc_rev_log.append(rev_log_mess)
+                            cr_area_top_level.append(top_level_names)
+                            title_file_name.append(changed_file_names)
         
-        count= count + 1
-        if temp_rev == reg_causing_revid:
-            reg_causing_revid_pos = count
+                            count= count + 1
+                            if temp_rev == reg_causing_revid:
+                                reg_causing_revid_pos = count
                   
             
-    #print "all docs=", all_docs , " pos=", reg_causing_revid_pos
-    tfidf_vectorizer = TfidfVectorizer(stop_words='english',decode_error='ignore()')
-    title_rev_log_tfidf_matrix = tfidf_vectorizer.fit_transform(title_rev_log)
-    desc_rev_log_tfidf_matrix = tfidf_vectorizer.fit_transform(desc_rev_log)
-    cr_area_top_level_tfidf_matrix = tfidf_vectorizer.fit_transform(cr_area_top_level)
-    title_file_name_tfidf_matrix = tfidf_vectorizer.fit_transform(title_file_name)
+                        #print "all docs=", all_docs , " pos=", reg_causing_revid_pos
+                        tfidf_vectorizer = TfidfVectorizer(stop_words='english',decode_error='ignore')
+                        title_rev_log_tfidf_matrix     = tfidf_vectorizer.fit_transform(title_rev_log)
+                        desc_rev_log_tfidf_matrix      = tfidf_vectorizer.fit_transform(desc_rev_log)
+                        cr_area_top_level_tfidf_matrix = tfidf_vectorizer.fit_transform(cr_area_top_level)
+                        title_file_name_tfidf_matrix   = tfidf_vectorizer.fit_transform(title_file_name)
     
-    print  "size=", title_rev_log_tfidf_matrix.shape,  desc_rev_log_tfidf_matrix.shape,  cr_area_top_level_tfidf_matrix.shape, title_file_name_tfidf_matrix.shape
+                        print  "size=", title_rev_log_tfidf_matrix.shape,  desc_rev_log_tfidf_matrix.shape,  cr_area_top_level_tfidf_matrix.shape, title_file_name_tfidf_matrix.shape
     
-    title_rev_log_sim_matrix = cosine_similarity(title_rev_log_tfidf_matrix[0:1], title_rev_log_tfidf_matrix)
-    desc_rev_log_sim_matrix= cosine_similarity(desc_rev_log_tfidf_matrix[0:1], desc_rev_log_tfidf_matrix)
-    cr_area_top_level_sim_matrix = cosine_similarity(cr_area_top_level_tfidf_matrix[0:1], cr_area_top_level_tfidf_matrix)
-    title_file_name_sim_matrix = cosine_similarity( title_file_name_tfidf_matrix[0:1],  title_file_name_tfidf_matrix)
+                        title_rev_log_sim_matrix = cosine_similarity(title_rev_log_tfidf_matrix[0:1], title_rev_log_tfidf_matrix)
+                        desc_rev_log_sim_matrix= cosine_similarity(desc_rev_log_tfidf_matrix[0:1], desc_rev_log_tfidf_matrix)
+                        cr_area_top_level_sim_matrix = cosine_similarity(cr_area_top_level_tfidf_matrix[0:1], cr_area_top_level_tfidf_matrix)
+                        title_file_name_sim_matrix = cosine_similarity( title_file_name_tfidf_matrix[0:1],  title_file_name_tfidf_matrix)
     
-    title_rev_log_sim       =  title_rev_log_sim_matrix[0][reg_causing_revid_pos]
-    desc_rev_log_sim        =  desc_rev_log_sim_matrix[0][reg_causing_revid_pos]
-    cr_area_top_level_sim   =  cr_area_top_level_sim_matrix[0][reg_causing_revid_pos]
-    title_file_name_sim     =  title_file_name_sim_matrix[0][reg_causing_revid_pos]
+                        title_rev_log_sim       =  title_rev_log_sim_matrix[0][reg_causing_revid_pos]
+                        desc_rev_log_sim        =  desc_rev_log_sim_matrix[0][reg_causing_revid_pos]
+                        cr_area_top_level_sim   =  cr_area_top_level_sim_matrix[0][reg_causing_revid_pos]
+                        title_file_name_sim     =  title_file_name_sim_matrix[0][reg_causing_revid_pos]
     
-    total_sim_reg_causing =  w1*title_rev_log_sim + w2*desc_rev_log_sim + w3*cr_area_top_level_sim + w4*title_file_name_sim
-    #print sim_matrix
+                        total_sim_reg_causing =  w1*title_rev_log_sim + w2*desc_rev_log_sim + w3*cr_area_top_level_sim + w4*title_file_name_sim
+                        #print sim_matrix
     
-    rank = get_rank(title_rev_log_sim_matrix, desc_rev_log_sim_matrix, cr_area_top_level_sim_matrix,title_file_name_sim_matrix, threshold, total_sim_reg_causing,w1, w2, w3, w4)
-    if rank<=threshold:
-       total_revids_found = total_revids_found +1
+                        rank = get_rank(title_rev_log_sim_matrix, desc_rev_log_sim_matrix, cr_area_top_level_sim_matrix,title_file_name_sim_matrix, threshold, total_sim_reg_causing,w1, w2, w3, w4)
+                        if rank<=threshold:
+                            total_revids_found = total_revids_found +1
     
 
-    break   
+                        break   
 
-print "Total bug found=", total_bugs
-print  "total revids =", total_revids_found
-precision =  (total_revids_found*100)/total_bugs
-print "precsion = ", precision
+                        print "Total bug found=", total_bugs
+                        print  "total revids =", total_revids_found
+                        precision =  (total_revids_found*100)/total_bugs
+                        print "precsion = ", precision
     
 
