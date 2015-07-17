443ca18332d543332b8eb8f1261b6711e1770229
SANGEETA <sangeeta.69b1@gmail.com>
2015-07-17 10:47:02 +0530

    indentation

--- regression1/src/regression1/vsm_ngram_regression.py
+++ regression1/src/regression1/vsm_ngram_regression.py
@@ -255,7 +255,7 @@ while w1 <=0.9:
                     print "precsion = ", precision
                          
                     insert_str =  "insert into "+learning_table+   " values ("+ (str)(w1)+","+ (str)(w2)+","+(str)(w3)+","+(str)(w4)+","+ model+","+(str)(threshold)\
-                        +","+(str)(precision)+","+(str)(total_bugs)+","+(str)(total_revids_found)+")" 
+                        +","+(str)(precision)+","+(str)(total_bugs)+","+(str)(total_revids_found)+ ")" 
                         
                     print "insert str=", insert_str
                     insert_cursor.execute(insert_str)
