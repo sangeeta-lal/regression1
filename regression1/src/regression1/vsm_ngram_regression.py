b856432333aa92dbbb6e4706a21f84847a8fdef9
SANGEETA <sangeeta.69b1@gmail.com>
2015-07-17 10:22:51 +0530

    @_

--- regression1/src/regression1/vsm_ngram_regression.py
+++ regression1/src/regression1/vsm_ngram_regression.py
@@ -37,7 +37,7 @@ bug_report_feature_table             = "temp"  + "_bug_report_features"
 bugid_previous_30_days_revids_table  = "temp"  +"_30_day"
 revid_feature_table                  = "temp"  +"_revid_feature"
 train_bugid_revid_table              = "temp"  
-learning_table = project +"_"+model+"weight_learning"
+learning_table = project +"_"+model+"_weight_learning"
 """
 port=3306
 user="root"
